75aa71e0f3b2eefe1df04d9d7345289658b6ee33
Basile <basile.marie67@gmail.com>
2025-05-12 11:28:44 +0200

    mise à jour de la grammaire pour inclure les doubles

--- nanoc.py
+++ nanoc.py
@@ -4,24 +4,28 @@ g = Lark("""
 IDENTIFIER: /[a-zA-Z_][a-zA-Z0-9]*/
 OPBIN: /[+\\-*\\/>]/
 NUMBER: /[1-9][0-9]*/ |"0"
+DOUBLE : /[0-9]+(\\.[0-9]+)?([eE][+-]?[0-9]+)?/
+CAST : "double" | "int"
 liste_var: ->vide
          |IDENTIFIER ("," IDENTIFIER)* ->vars
 expression: IDENTIFIER ->var
          | expression OPBIN expression ->opbin
+         | DOUBLE -> double
          | NUMBER ->number
 command: command ";" (command)* ->sequence
          |"while" "(" expression ")" "{" command "}" ->while
          |IDENTIFIER "=" expression ->affectation
+         |IDENTIFIER "=""("CAST")" expression -> casting
          |"if" "(" expression ")" "{" command "}" ("else" "{" command "}")? ->ite
          |"printf" "(" expression ")" ->print
          |"skip" ->skip
 program: "main" "(" liste_var ")" "{" command "return" "(" expression")" "}" ->main
 %import  common.WS
 %ignore WS
-""", start='program')
+""", start='command')
 
 def pp_expression(e):
-    if e.data in ['var','number'] : return f"{e.children[0].value}"
+    if e.data in ['var','number','double'] : return f"{e.children[0].value}"
     if e.data == "parentheses": return f"({pp_expression(e.children[0])}) {e.children[1].value} {pp_expression(e.children[2])}"
     if e.data != "parentheses":
         e_left = e.children[0]
@@ -33,37 +37,23 @@ def pp_commande(c):
     if c.data == "affectation":
         var = c.children[0]
         exp = c.children[1]
-        return f"{var.value} = {pp_expression(exp)}"
+        return f"{var.value} = {pp_expression(exp)};"
+    if c.data == "casting" :
+        var = c.children[0]
+        cast = c.children[1]
+        exp = c.children[2]
+        return f"{var.value} = ({cast.value}) {pp_expression(exp)}"
     elif c.data == "skip": return "skip"
     elif c.data == "print": return f"print({pp_expression(c.children[0])})"
     elif c.data == "while":
         exp = c.children[0]
         body = c.children[1]
-        return f"while ({pp_expression(exp)}) {{{pp_commande(body)}}}"
+        return f"while {pp_expression(exp)}\n  {{{pp_commande(body)}}}"
     elif c.data == "sequence":
         head = c.children[0]
         tail = c.children[1]
-        return f"{pp_commande(head)};\n{pp_commande(tail)}"
+        return f"{pp_commande(head)} ; {pp_commande(tail)}"
 
-    return ""
-
-def pp_list_var(lv):
-    list_var = ""
-    for v in lv:
-        list_var += v
-        list_var += ", "
-
-    return list_var[: -2]
-
-def pp_programme(p):
-    list_var = pp_list_var(p.children[0].children)
-    commands = pp_commande(p.children[1])
-    retour = pp_expression(p.children[2])
-    corps = f"""{commands}
-    return({retour})
-    """
-    return f"""main({list_var}) {{
-    {corps}}} """
 
 op2asm = {'+': "add rax, rbx", "-": " sub rax, rbx"}
 cpt = iter(range(1000000))
@@ -78,14 +68,15 @@ def asm_expression(e):
         e_right = e.children[2]
         asm_left = asm_expression(e_left)
         asm_right = asm_expression(e_right)
-        return f"""{asm_left}
+    return f"""{asm_left}
 push rax
 {asm_right}
 mov rbx, rax
 pop rax
 {op2asm[e_op.value]}\n"""
-
     
+
+
 def asm_commande(c):
     if c.data == "affectation":
         var = c.children[0]
@@ -139,13 +130,17 @@ def asm_programme(p):
 
 
 if __name__ == "__main__":
-    with open("simple.c") as f:
+    with open("sample.c") as f:
         src = f.read()
-    ast = g.parse(src)
-    res = asm_programme(ast)
-    print(pp_programme(ast))
-    with open("simple.asm", "w") as result:
-        result.write(res)
+        ast = g.parse('X = (double) 3.898989')
+        res = pp_commande(ast)
+        print(res)
+
+        #ast = g.parse(src)
+        #res = asm_programme(ast)
+        #print(res)
+    #with open("sample.asm", "w") as result:
+        #result.write(res)
 
     #print(ast.pretty('  '))
 #print(ast.data)
