d88ba683737619ab9d8c00eed434eb9557f089ad
MathusLXX <jeannotmathieu70@gmail.com>
2025-06-10 11:29:11 +0200

    added sample_struct.c

---
+++ sample_struct.c
@@ -0,0 +1,24 @@
+
+typedef struct{
+    int x;
+    int y;
+    }Point;    
+
+typedef struct{
+    Point A;
+    Point B;
+}Ligne;
+
+int main(int X, int Y) {
+    Point P;
+    Point Q;
+    Q.x = 1;
+    Q.y = 1;
+    P.x = X + Y;
+    P.y = 1;
+    P.x = P.x + P.y;
+    Ligne L;
+    L.A = P;
+    L.B = Q;
+    return(P.x)
+}
