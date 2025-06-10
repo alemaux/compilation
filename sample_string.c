0db31364161991fcab816a66e9125ada945adaf5
raphael-franco <raphfranco@free.fr>
2025-06-10 18:14:34 +0200

    ajustement de la feature string sur le main

---
+++ sample_string.c
@@ -0,0 +1,23 @@
+string main(long X, long Y, string w, string concat, string test) {
+    int A = 13;
+    string h = "hello";
+    int l = len(h);
+    printf(l);
+    printf(45);
+    printf(h);
+    w = "world";
+    printf(w);
+    int length = len(w);
+    printf(length);
+    concat = h + w;
+    printf(concat);
+    test = concat + h;
+    printf(test);
+    int ind = h[1];
+    printf(ind);
+    while(X){
+        X= X - 1;
+        Y = Y + 1;
+    };
+    return (h)
+}
